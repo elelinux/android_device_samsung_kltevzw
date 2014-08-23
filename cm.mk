ed3f3bc71b3fc70921c09e908a70460f535e95fa
elelinux <ele.linux@gmail.com>
2014-08-23 17:09:09 +0200

    add bootanimation size

--- cm.mk
+++ cm.mk
@@ -20,6 +20,10 @@
 # definition file).
 #
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 $(call inherit-product, device/samsung/klteusc/full_kltevzw.mk)
 
 # Inherit some common CM stuff.
